7e40ea23ac4f14246a61b74662511ec942824021
ambijani <a.bijani@wustl.edu>
2023-11-03 19:39:57 +0000

    added styling to new home screen and refactored petition section

--- index.js
+++ index.js
@@ -13,21 +13,58 @@ const counter = document.getElementById("counter");
 let count = 3;
 
 const addSignature = () => {
-  const name = document.getElementById("name").value;
-  const hometown = document.getElementById("hometown").value;
+  const nameInput = document.getElementById("name");
+  const hometownInput = document.getElementById("hometown");
+  const emailInput = document.getElementById("email");
 
-  // Create a new signature paragraph
-  const newSignature = document.createElement("p");
-  newSignature.textContent = `🖊️ ${name} from ${hometown} supports this.`;
+  const name = nameInput.value;
+  const hometown = hometownInput.value;
+  const email = emailInput.value;
 
-  // Append the new signature to the signatures div
-  signatures.appendChild(newSignature);
+  if (name.length < 2 || hometown.length < 2 || (!email.includes('.com') && !email.includes('.edu') && !email.includes('.org'))) {
+    // Highlight invalid input fields with error class
+    if (name.length < 2) {
+      nameInput.classList.add('error');
+    } else {
+      nameInput.classList.remove('error');
+    }
+    if (hometown.length < 2) {
+      hometownInput.classList.add('error');
+    } else {
+      hometownInput.classList.remove('error');
+    }
+    if (!email.includes('.com')) {
+      emailInput.classList.add('error');
+    } else {
+      emailInput.classList.remove('error');
+    }
 
-  // Increase the count
-  count++;
+    // Show an alert for invalid signature
+    alert("Invalid signature. Please make sure all fields are correctly filled.");
+  } else {
+    // Create a new signature paragraph
+    const newSignature = document.createElement("p");
+    newSignature.textContent = `🖊️ ${name} from ${hometown} supports this.`;
 
-  // Update the counter
-  counter.textContent = `🖊️ ${count} people have signed this petition and support this cause.`;
+    // Append the new signature to the signatures div
+    signatures.appendChild(newSignature);
+
+    // Increase the count
+    count++;
+
+    // Update the counter
+    counter.textContent = `🖊️ ${count} people have signed this petition and support this cause.`;
+
+    // Clear form fields
+    nameInput.value = "";
+    hometownInput.value = "";
+    emailInput.value = "";
+
+    // Remove error class from input fields
+    nameInput.classList.remove('error');
+    hometownInput.classList.remove('error');
+    emailInput.classList.remove('error');
+  }
 };
 
 signNowButton.addEventListener("click", addSignature);
